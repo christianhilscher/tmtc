19b7f6ef9bc0f8b19230fa450d1d0a344d14f489
LCruzFer <lucas.cruz@hotmail.de>
2020-07-27 18:19:52 +0200

    prelim results, file under construction

--- src/analysis/triple_counter_v2.py
+++ src/analysis/triple_counter_v2.py
@@ -8,6 +8,13 @@ wd_lc = "/Users/llccf/OneDrive/Dokumente/tmtc/"
 wd_ch = "/Users/christianhilscher/Desktop/tmtc/"
 os.chdir(wd_lc)
 
+#functions 
+def get_first(number, first = 4): 
+    """
+    Get first 'first' digits of supplied 'number'.
+    """
+    return(int(str(number)[:4]))
+
 #read in all data
 #citations
 cites = pd.read_csv("src/get_data/data/tables/grant_cite.csv")
@@ -31,37 +38,38 @@ merged = merged.drop(['patnum', 'pubdate'], axis = 1)
 #merge the new df with main AGAIN, but this time based on dst to get firm 
 #names of dst owners
 merged_2 = merged.merge(main_red, left_on = 'dst', right_on = 'patnum', how = 'inner')
-merged_2 = merged_2.rename(columns = {'owner': 'owner_dst'})
-#drop all rows which have no dst owners since they are of no use 
-matched = merged_2.dropna(how = 'any').drop('patnum', axis = 1)
+matched = merged_2.rename(columns = {'owner': 'owner_dst'})
 #turn publication date into year only 
-def get_first(number, first = 4): 
-    return(int(str(number)[:4]))
 matched['pubdate'] = matched['pubdate'].apply(get_first, args = [4])
-#drop everything where owner_src == owner_dst
-matched = matched[matched['owner_src'] != matched['owner_dst']]
-#we can also drop all observations in where owner_dst is not in 
-#owner_src, since in such a case a mutual directed edge is not possible 
-#but first get total number of references in 2010
-tot_ref = len(matched[matched['pubdate'] == 2010])
-matched = matched[matched['owner_dst'].isin(matched['owner_src'])].reset_index(drop = True)
 
 #!Network Analysis
 thicket_dict = {}
+citations_dict = {}
+triangle_dict = {}
 for year in range(1990, 2001):
+    year = 1992
     matched_year = matched[matched['pubdate'] <= year]
+    tot_ref = len(matched_year)
+    #only keep src that is also in dst, otherwise not possible to have mutually directed edges
+    matched_year = matched_year[matched_year['owner_dst'].isin(matched_year['owner_src'])].reset_index(drop = True)
+    #drop where dst is not known, aka all rows that have owner_dst as nan
+    matched_year = matched_year.dropna(how = 'any').drop('patnum', axis = 1)
+    #create graph 
     G = nx.DiGraph()
     #add nodes 
-    owners_src = matched_year['owner_src'].tolist() 
+    owners_src = matched_year['owner_src'].tolist()
     owners_dst = matched_year['owner_dst'].tolist()
     nodes = owners_src + owners_dst
-    #remove duplicates of firms 
+    #remove duplicates of firms
     nodes = list(dict.fromkeys(nodes))
     G.add_nodes_from(nodes)
+    #add edges
     edges = list(zip(matched_year['owner_src'], matched_year['owner_dst']))
     edges = list(set([i for i in edges]))
     G.add_edges_from(edges)
-
+    nodes_dict = {}
+    adj_mat = nx.adjacency_matrix(G)
+    adj_mat_dense = adj_mat.todense()
     node_dict = {}
     for x in list(G.nodes):
         neigh = list(G.neighbors(x))
@@ -75,10 +83,11 @@ for year in range(1990, 2001):
     for k, v in list(node_dict.items()): 
         if v == []: 
             del node_dict[k]
-
+    
     G_new = nx.Graph(node_dict)
     tris = list(nx.triangles(G_new).values())
     tris_arr = np.array(tris)
     tris_sum = np.sum(tris_arr)/3
-    tot_ref = len(matched_year)
+    triangle_dict[str(year)] = tris_sum 
+    citations_dict[str(year)] = tot_ref
     thicket_dict[str(year)] = tris_sum/tot_ref
